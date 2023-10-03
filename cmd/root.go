45a9c113347bb6eb3488b9c890855f631f0997d0
Martin Haram Nygård <martin.nygard@kartverket.no>
2023-10-03 12:10:18 +0200

    Add log statement for config

--- cmd/root.go
+++ cmd/root.go
@@ -38,8 +38,9 @@ func initConfig() {
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.SetEnvPrefix("SL")
 	viper.AutomaticEnv() // read in environment variables that match
-
 	// If a config file is found, read it in.
+	fmt.Fprintln(os.Stdout, "Looking to dir for config:", home)
+
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
